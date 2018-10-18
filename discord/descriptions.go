7282ddcf36bf0078f6f3830af7324e68359afe4e
Aaron Scherer <aequasi@gmail.com>
2018-10-18 16:47:37 -0700

    Adding channel resource

--- discord/descriptions.go
+++ discord/descriptions.go
@@ -4,8 +4,16 @@ var descriptions map[string]string
 
 func init() {
 	descriptions = map[string]string{
+		// Provider
 		"discord_provider_token":        "The token to use for authenticating with the Discord API",
+
+		// Server
 		"discord_resource_server_name":  "What to name the new server",
 		"discord_resource_server_empty": "Whether or not the server should be created with no channels",
+
+		// Channel
+		"discord_resource_channel_guild": "What guild this channel belongs to",
+		"discord_resource_channel_name":  "What to name the new channel",
+		"discord_resource_channel_type":  "What type of channel is this (text, voice, category)",
 	}
 }
---
+++ discord/resource_discord_channel.go
@@ -0,0 +1,113 @@
+package discord
+
+import (
+	"errors"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceDiscordChannel() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceChannelCreate,
+		Read:   resourceChannelRead,
+		Update: resourceChannelUpdate,
+		Delete: resourceChannelDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: descriptions["discord_resource_channel_name"],
+			},
+			"guild": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: descriptions["discord_resource_channel_guild"],
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Default:  "text",
+				Optional: true,
+				ForceNew: true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errors []error) {
+					v := val.(string)
+					if v != "text" && v != "voice" && v != "category" {
+						errors = append(errors, fmt.Errorf("%q must be one of: text, voice, category, got: %d", key, v))
+					}
+
+					return
+				},
+				Description: descriptions["discord_resource_channel_type"],
+			},
+		},
+	}
+}
+
+func resourceChannelCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*discordgo.Session)
+
+	guildId := d.Get("guild").(string)
+	guild, err := client.Guild(guildId)
+	if err != nil {
+		return errors.New("Guild does not exist with that ID: " + guildId)
+	}
+
+	name := d.Get("name").(string)
+	channelType := d.Get("type").(string)
+	channel, err := client.GuildChannelCreate(guild.ID, name, channelType)
+	if err != nil {
+		return errors.New("Failed to create a channel: " + err.Error())
+	}
+
+	d.SetId(channel.ID)
+
+	return nil
+}
+
+func resourceChannelRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*discordgo.Session)
+
+	channel, err := client.Channel(d.Id())
+	if err != nil {
+		d.SetId("")
+
+		return nil
+	}
+
+	d.Set("type", channel.Type)
+	d.Set("name", channel.Name)
+
+	return nil
+}
+
+func resourceChannelUpdate(d *schema.ResourceData, m interface{}) error {
+	d.Partial(true)
+	client := m.(*discordgo.Session)
+
+	if d.HasChange("name") {
+		_, err := client.ChannelEdit(d.Id(), d.Get("name").(string))
+		if err != nil {
+			return err
+		}
+
+		d.SetPartial("name")
+	}
+
+	d.Partial(false)
+
+	return nil
+}
+
+func resourceChannelDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*discordgo.Session)
+
+	_, err := client.Channel(d.Id())
+	if err != nil {
+		return nil
+	}
+
+	_, _ = client.ChannelDelete(d.Id())
+
+	return nil
+}
