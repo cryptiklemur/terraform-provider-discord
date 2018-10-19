9548ba3bb18ef2d6c7008674938151bc3e0f8397
Aaron Scherer <aequasi@gmail.com>
2018-10-18 17:20:08 -0700

    Adding invite resource

--- discord/descriptions.go
+++ discord/descriptions.go
@@ -5,7 +5,7 @@ var descriptions map[string]string
 func init() {
 	descriptions = map[string]string{
 		// Provider
-		"discord_provider_token":        "The token to use for authenticating with the Discord API",
+		"discord_provider_token": "The token to use for authenticating with the Discord API",
 
 		// Server
 		"discord_resource_server_name":  "What to name the new server",
@@ -15,5 +15,12 @@ func init() {
 		"discord_resource_channel_guild": "What guild this channel belongs to",
 		"discord_resource_channel_name":  "What to name the new channel",
 		"discord_resource_channel_type":  "What type of channel is this (text, voice, category)",
+
+		// Invite
+		"discord_resource_invite_channel":   "What channel this invite belongs to",
+		"discord_resource_invite_max_age":   "Duration of invite in seconds before expiry, or 0 for never",
+		"discord_resource_invite_max_uses":  "Max number of uses or 0 for unlimited",
+		"discord_resource_invite_temporary": "Whether this invite only grants temporary membership",
+		"discord_resource_invite_unique":    "if true, don't try to reuse a similar invite (useful for creating many unique one time use invites)",
 	}
 }
---
+++ discord/resource_discord_invite.go
@@ -0,0 +1,107 @@
+package discord
+
+import (
+	"errors"
+	"github.com/bwmarrin/discordgo"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceDiscordInvite() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceInviteCreate,
+		Read:   resourceInviteRead,
+		Update: resourceInviteUpdate,
+		Delete: resourceInviteDelete,
+
+		Schema: map[string]*schema.Schema{
+			"channel": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: descriptions["discord_resource_invite_channel"],
+			},
+			"max_age": {
+				Type:        schema.TypeInt,
+				ForceNew:    true,
+				Optional:    true,
+				Default:     86400,
+				Description: descriptions["discord_resource_invite_max_age"],
+			},
+			"max_uses": {
+				Type:        schema.TypeInt,
+				ForceNew:    true,
+				Optional:    true,
+				Default:     0,
+				Description: descriptions["discord_resource_invite_max_uses"],
+			},
+			"temporary": {
+				Type:        schema.TypeBool,
+				ForceNew:    true,
+				Optional:    true,
+				Default:     false,
+				Description: descriptions["discord_resource_invite_temporary"],
+			},
+			"unique": {
+				Type:        schema.TypeBool,
+				ForceNew:    true,
+				Optional:    true,
+				Default:     false,
+				Description: descriptions["discord_resource_invite_unique"],
+			},
+		},
+	}
+}
+
+func resourceInviteCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*discordgo.Session)
+
+	channelId := d.Get("channel").(string)
+	channel, err := client.Channel(channelId)
+	if err != nil {
+		return errors.New("Channel does not exist with that ID: " + channelId)
+	}
+
+	invite, err := client.ChannelInviteCreate(channel.ID, discordgo.Invite{
+		MaxAge:    d.Get("max_age").(int),
+		MaxUses:   d.Get("max_uses").(int),
+		Temporary: d.Get("temporary").(bool),
+		Unique:    d.Get("unique").(bool),
+	})
+	if err != nil {
+		return errors.New("Failed to create a invite: " + err.Error())
+	}
+
+	d.SetId(invite.Code)
+
+	return nil
+}
+
+func resourceInviteRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*discordgo.Session)
+
+	_, err := client.Invite(d.Id())
+	if err != nil {
+		d.SetId("")
+
+		return nil
+	}
+
+	return nil
+}
+
+func resourceInviteUpdate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceInviteDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*discordgo.Session)
+
+	_, err := client.Invite(d.Id())
+	if err != nil {
+		return nil
+	}
+
+	_, _ = client.InviteDelete(d.Id())
+
+	return nil
+}
